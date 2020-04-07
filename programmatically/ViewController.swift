fff4a136a0ab777a56a13b591298c6d075bf41ac
den0206 <mac.pop@mineo.jp>
2020-04-07 22:11:03 +0900

    Initial Commit

---
+++ programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programmatically
+//
+//  Created by 酒井ゆうき on 2020/04/07.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
